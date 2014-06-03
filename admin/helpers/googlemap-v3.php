78d29037c38bc7d2dc8c9181b6275f5cf53d97ce
DEGENNES Charles-Antoine <cadegenn@gmail.com>
2014-06-03 20:52:21 +0200

    small enhancement that do not do the job better

--- admin/helpers/googlemap-v3.php
+++ admin/helpers/googlemap-v3.php
@@ -4,16 +4,29 @@
  * 
  */
 class GMAPv3{
+	/*
+	 * url of google geocode api
+	 */
 	static private $url = "http://maps.google.com/maps/api/geocode/json?sensor=false";
+	/*
+	 * error_message returned by query
+	 */
+	static public $error_message = "";
+	/*
+	 * status code of the query
+	 */
+	static public $status = "";
 
 	static public function geocode($url){
 		$resp_json = self::curl_file_get_contents($url);
 		//$resp_json = file_get_contents($url);
 		$resp = json_decode($resp_json, true);
-
-		if($resp['status']='OK'){
+		self::$status = $resp['status'];
+		
+		if (self::$status == 'OK') {
 			return $resp['results'][0];
-		}else{
+		} else {
+			self::$error_message = $resp['error_message'];
 			return false;
 		}
 	}
@@ -63,10 +76,14 @@ class GMAPv3{
 
 	static private function curl_file_get_contents($URL){
 		$c = curl_init();
+		curl_setopt($c, CURLOPT_HEADER, 0);
+		curl_setopt($c, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+		curl_setopt($c, CURLOPT_FOLLOWLOCATION, 1);
 		curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($c, CURLOPT_URL, $URL);
 		$contents = curl_exec($c);
 		curl_close($c);
+		//echo("<pre>");		var_dump($contents); echo("</pre>");
 
 		if ($contents) return $contents; else return FALSE;
 	}
